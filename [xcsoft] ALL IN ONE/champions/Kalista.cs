3f08fe29b016ddc26fed3d69d1fcb9bece33bc9c
xcsoftforlsharp <xcsoft@leaguesharp.xcsoft.pusku.com>
2015-03-26 07:42:33 +0900

    work in progress

--- [xcsoft] ALL IN ONE/champions/Kalista.cs
+++ [xcsoft] ALL IN ONE/champions/Kalista.cs
@@ -19,15 +19,15 @@ namespace _xcsoft__ALL_IN_ONE.champions
 
         public static void Load()
         {
-            Q = new Spell(SpellSlot.Q, 1180f);
+            Q = new Spell(SpellSlot.Q, 1150f);
             W = new Spell(SpellSlot.W, 5200f);
             E = new Spell(SpellSlot.E, 1000f);
             R = new Spell(SpellSlot.R, 1400f);
 
-            Q.SetSkillshot(0.25f, 30f, 1700f, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 40f, 1700f, true, SkillshotType.SkillshotLine);
 
             var drawDamageMenu = new MenuItem("Draw_RDamage", "Draw (E) Damage", true).SetValue(true);
-            var drawFill = new MenuItem("Draw_Fill", "Draw (E) Damage Fill", true).SetValue(new Circle(true, Color.FromArgb(90, 255, 169, 4)));
+            var drawFill = new MenuItem("Draw_Fill", "Draw (E) Damage Fill", true).SetValue(new Circle(true, Color.Red));
 
             Menu.SubMenu("Combo").AddItem(new MenuItem("comboUseQ", "Use Q", true).SetValue(true));
             Menu.SubMenu("Combo").AddItem(new MenuItem("comboUseE", "Use E", true).SetValue(true));
@@ -108,22 +108,22 @@ namespace _xcsoft__ALL_IN_ONE.champions
             if (Player.IsDead)
                 return;
 
-            var drawingQ = Menu.Item("drawQ", true).GetValue<Circle>();
-            var drawingW = Menu.Item("drawW", true).GetValue<Circle>();
-            var drawingE = Menu.Item("drawE", true).GetValue<Circle>();
-            var drawingR = Menu.Item("drawR", true).GetValue<Circle>();
+            var drawQ = Menu.Item("drawQ", true).GetValue<Circle>();
+            var drawW = Menu.Item("drawW", true).GetValue<Circle>();
+            var drawE = Menu.Item("drawE", true).GetValue<Circle>();
+            var drawR = Menu.Item("drawR", true).GetValue<Circle>();
 
-            if (Q.IsReady() && drawingQ.Active)
-                Render.Circle.DrawCircle(Player.Position, Q.Range, drawingQ.Color);
+            if (Q.IsReady() && drawQ.Active)
+                Render.Circle.DrawCircle(Player.Position, Q.Range, drawQ.Color);
 
-            if (W.IsReady() && drawingW.Active)
-                Render.Circle.DrawCircle(Player.Position, W.Range, drawingW.Color);
+            if (W.IsReady() && drawW.Active)
+                Render.Circle.DrawCircle(Player.Position, W.Range, drawW.Color);
 
-            if (E.IsReady() && drawingE.Active)
-                Render.Circle.DrawCircle(Player.Position, E.Range, drawingE.Color);
+            if (E.IsReady() && drawE.Active)
+                Render.Circle.DrawCircle(Player.Position, E.Range, drawE.Color);
 
-            if (R.IsReady() && drawingR.Active)
-                Render.Circle.DrawCircle(Player.Position, R.Range, drawingR.Color);
+            if (R.IsReady() && drawR.Active)
+                Render.Circle.DrawCircle(Player.Position, R.Range, drawR.Color);
         }
 
         static void Obj_AI_Hero_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
@@ -131,15 +131,12 @@ namespace _xcsoft__ALL_IN_ONE.champions
             if (sender.IsMe && args.SData.Name == "KalistaExpungeWrapper")
                     Utility.DelayAction.Add(250, Orbwalking.ResetAutoAttackTimer);
 
-            if (Menu.Item("soulboundsaver", true).GetValue<Boolean>() && R.IsReady())
+            if (Menu.Item("soulboundsaver", true).GetValue<Boolean>() && sender.Type == GameObjectType.obj_AI_Hero && sender.IsEnemy && R.IsReady())
             {
-                if (sender.Type == GameObjectType.obj_AI_Hero && sender.IsEnemy)
-                {
-                    var soulboundhero = HeroManager.Allies.FirstOrDefault(hero => hero.HasBuff("kalistacoopstrikeally", true) && args.Target.NetworkId == hero.NetworkId && hero.HealthPercentage() <= 15);
+                var soulbound = HeroManager.Allies.FirstOrDefault(hero => hero.HasBuff("kalistacoopstrikeally", true) && args.Target.NetworkId == hero.NetworkId && hero.HealthPercentage() <= 20);
 
-                    if (R.CanCast(soulboundhero))
-                        R.Cast();
-                }
+                if (soulbound != null)
+                    R.Cast();
             }
         }
 
@@ -268,11 +265,8 @@ namespace _xcsoft__ALL_IN_ONE.champions
 
                     if (killcount >= Menu.Item("laneclearQnum", true).GetValue<Slider>().Value)
                     {
-                        if (!Player.IsWindingUp && !Player.IsDashing())
-                        { 
-                            Q.Cast(minion);
-                            break;
-                        }
+                        Q.Cast(minion);
+                        break;
                     }
                 }
                 //-------------------------------------------------------------------------------------------------------------------------------
