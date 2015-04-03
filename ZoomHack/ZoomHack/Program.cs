bc44aea1e3a9246e2c0ffdc181a303a5b39e5593
xcsoftforlsharp <xcsoft@leaguesharp.xcsoft.pusku.com>
2015-04-04 00:18:11 +0900

    kappa

---
+++ ZoomHack/ZoomHack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace ZoomHack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Hacks.ZoomHack = true;
+        }
+    }
+}
